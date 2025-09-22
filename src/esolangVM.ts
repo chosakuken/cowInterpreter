f1e70b13a587fdaa3a894c210bc82592f3d175de
chosakuken <shunpc2018@outlook.jp>
2025-09-22 15:59:52 +0900

    done VM

--- src/esolangVM.ts
+++ src/esolangVM.ts
@@ -1,58 +1,56 @@
 // VMは Singletonとして設計
 class VM {
-    // メンバ
-    private static instance: VM;
-    private address: number;
-    private register: number | null;
-    private memory: Map<number, number>;
-    private constructor() {
-        this.address = 0;
-        this.register = null;
-        this.memory = new Map<number, number>();
+  // メンバ
+  private static instance: VM | null;
+  private address: number;
+  private register: number | null;
+  private memory: Map<number, number>;
+  private constructor() {
+    this.address = 0;
+    this.register = null;
+    this.memory = new Map<number, number>();
+  }
+  public static getInstance(): VM {
+    this.instance ??= new VM();
+    return this.instance;
+  }
+  // VM の初期化
+  public initVM(): void {
+    this.address = 0;
+    this.register = null;
+    this.memory.set(this.address, 0);
+  }
+  // メモリの読み出し
+  public readMemory(): number {
+    const memoryValue = this.memory.get(this.address);
+    if (memoryValue === undefined) {
+      throw new Error("Current memory undefined");
     }
-    public static getInstance(): VM {
-        if (this.instance === null) {
-            this.instance = new VM();
-        }
-        return this.instance;
+    return memoryValue;
+  }
+  // メモリへの書き出し
+  public writeMemory(input: number): void {
+    this.memory.set(this.address, input);
+  }
+  // アドレス読み出し
+  public readAddress(): number {
+    return this.address;
+  }
+  // アドレス変更
+  public writeAddress(input: number): void {
+    this.address = input;
+    if (this.memory.get(this.address) === undefined) {
+      this.memory.set(this.address, 0);
     }
-    public initVM(): void {
-        this.address = 0;
-        this.register = null;
-        this.memory.set(this.address, 0);
-    }
-    // ブロックを1つ前に戻す
-    public movebackBlock(): void {
-        this.address -= 1;
-        if (this.memory.get(this.address) === undefined) {
-            this.memory.set(this.address, 0);
-        }
-    }
-    // ブロックを1つ後に進める
-    public moveforwardBlock(): void {
-        this.address += 1;
-        if (this.memory.get(this.address) === undefined) {
-            this.memory.set(this.address, 0);
-        }
-    }
-    // メモリをデクリメントする
-    public decrementMemory(): void {
-        const memoryValue = this.memory.get(this.address);
-        if (memoryValue === undefined) {
-            throw new Error("Current Memory Undefined");
-        }
-        this.memory.set(this.address, memoryValue - 1)
-    }
-    // メモリをインクリメントする
-    public incrementMemory(): void {
-        const memoryValue = this.memory.get(this.address);
-        if (memoryValue === undefined) {
-            throw new Error("Current Memory Undefined");
-        }
-        this.memory.set(this.address, memoryValue + 1)
-    }
-    // 現在のメモリを 0 に設定
-    public setZero(): void {
-        this.memory.set(this.address, 0);
-    }
+  }
+  // レジスタ読み出し
+  public readRegister(): number | null {
+    return this.register;
+  }
+  // レジスタ書き込み
+  public writeRegister(input: number | null): void {
+    this.register = input;
+  }
 }
+
+export { VM };
